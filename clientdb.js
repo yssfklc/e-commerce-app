990ca077ca29400264b52bd63f43a6251558d1ae
Yusuf Kılıç <y.kilic@superpay.tech>
2023-09-25 20:26:28 +0300

    edit url for production

--- clientdb.js
+++ clientdb.js
@@ -1,13 +1,18 @@
 const dotenv = require('dotenv');
 const Pool=require('pg').Pool;
 dotenv.config();
-const pool=new Pool ({
+
+const devconfig = {
     user:process.env.user,
     host:process.env.host,
     database:process.env.database,
     password:process.env.password,
     port:process.env.port
-});
+}
+const proconfig = {
+    connectionString: process.env.DATABASE_URL,
+}
+const pool=new Pool (process.env.NODE_ENV === "production"? proconfig : devconfig);
 
 module.exports = {
     pool
