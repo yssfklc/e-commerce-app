469f123194636c3458e58058cc1225f8e82369bc
Yusuf Kılıç <y.kilic@superpay.tech>
2023-09-26 18:39:11 +0300

    Fix service url

--- index.js
+++ index.js
@@ -17,7 +17,7 @@ app.use(cors());
 
 app.use(express.static("client/build"));
 if(process.env.NODE_ENV === 'production'){
-  app.use(express.static(path.join(__dirname + "client/build")));
+  app.use(express.static(path.join(__dirname, "client/build")));
 }
 app.use(express.urlencoded({ extended: false }));
 
