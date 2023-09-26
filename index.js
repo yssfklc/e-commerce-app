5677f1f28b671ed61af0822ad8985163a61b751c
Yusuf Kılıç <y.kilic@superpay.tech>
2023-09-26 18:38:33 +0300

    Fix login bug

--- index.js
+++ index.js
@@ -15,11 +15,11 @@ const cors = require('cors');
 app.use(express.json());
 app.use(cors());
 
-app.use(express.urlencoded({ extended: false }));
 app.use(express.static("client/build"));
 if(process.env.NODE_ENV === 'production'){
   app.use(express.static(path.join(__dirname + "client/build")));
 }
+app.use(express.urlencoded({ extended: false }));
 
 app.use(
   session({
