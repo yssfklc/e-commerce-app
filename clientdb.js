128ac69b7ce0f6e5b58b8fd08bc7a691f406b0c6
Yusuf Kılıç <y.kilic@superpay.tech>
2023-09-26 19:55:28 +0300

    Fix service bug

--- clientdb.js
+++ clientdb.js
@@ -11,8 +11,11 @@ const devconfig = {
 }
 const proconfig = {
     connectionString: process.env.DATABASE_URL,
+    ssl: {
+        rejectUnauthorized: false,
+    }
 }
-const pool=new Pool (process.env.NODE_ENV === "production"? proconfig : devconfig);
+const pool=new Pool(process.env.NODE_ENV === "production" ? proconfig : devconfig);
 
 module.exports = {
     pool
